6665d0cdeef8b6fd2b70f7b5fe4e79ae71fc8a62
David Isakson <ikeman32@yahoo.com>
2019-12-18 13:12:41 -0800

    Initial commit

---
+++ server-dev-token.sh
@@ -0,0 +1,12 @@
+npm init
+npm i bcryptjs cors express helmet jsonwebtoken knex sqlite3 && 
+npm i nodemon --save-dev &&
+mkdir api &&
+touch api/server.js &&
+mkdir auth &&
+touch auth/auth-router.js auth/restricted-middleware.js &&
+mkdir database &&
+touch database/dbConfig.js &&
+mkdir users &&
+touch users/users-model.js users/users-router.js &&
+knex init
